5e6875e0b1a61a566bcd478213246be94e7dabeb
Shah Faisal Khan <shahfaisal3102@gmail.com>
2026-05-08 12:07:27 +0530

    fix error

--- src/app/components/hero/hero.ts
+++ src/app/components/hero/hero.ts
@@ -2,7 +2,6 @@ import { CommonModule } from '@angular/common';
 import { Component, AfterViewInit, ElementRef, ViewChild } from '@angular/core';
 import Typed from 'typed.js'
 import { RouterLink } from "@angular/router";
-import { ButtonIcon } from "primeng/button";
 import { Bind } from "../../../../../node_modules/primeng/types/primeng-bind";
 
 @Component({
