22385bc42b0d5e768b67fb814128017641b0c5fa
Shah Faisal Khan <shahfaisal3102@gmail.com>
2026-05-08 15:10:14 +0530

    fix changes

--- src/app/app.routes.ts
+++ src/app/app.routes.ts
@@ -1,9 +1,3 @@
 import { Routes } from '@angular/router';
-import { Hero } from './components/hero/hero';
-import { About } from './components/about/about';
-import { Experience } from './components/experience/experience';
-import { Skills } from './components/skills/skills';
-import { Project } from './shared/services/project';
-import { Projects } from './components/projects/projects';
 
 export const routes: Routes = []
